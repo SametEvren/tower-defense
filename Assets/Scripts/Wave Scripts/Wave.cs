b57592dcdaccaf2ba1215908b9549e7ea68b9ad9
Samet <69472552+SametEvren@users.noreply.github.com>
2024-06-04 19:00:05 +0300

    feat: Prefabs and animators created.

---
+++ Assets/Scripts/Wave Scripts/Wave.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Wave_Scripts
+{
+    [CreateAssetMenu(fileName = "Wave", menuName = "Wave/New Wave")]
+    public class Wave : ScriptableObject
+    {
+        
+    }
+}
