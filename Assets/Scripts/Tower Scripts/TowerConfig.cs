b42f3d59a468147f8493a2ecf5bf31de2a0e4a20
Samet <69472552+SametEvren@users.noreply.github.com>
2024-06-05 02:52:18 +0300

    feat: Tower Data created.

---
+++ Assets/Scripts/Tower Scripts/TowerConfig.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Tower_Scripts
+{
+    [CreateAssetMenu(fileName = "Tower Config", menuName = "Tower/Tower Config")]
+    public class TowerConfig : ScriptableObject
+    {
+        public GameObject projectile;
+        public Sprite sprite;
+        public string description;
+        public float range;
+        public float damage;
+        public float fireRate;
+        public DamageType damageType;
+        public int cost;
+    }
+
+    public enum DamageType
+    {
+        Area,
+        Single
+    }
+}
